4658e18165700eab30bc0c2cef9b2448743330a3
HamidNE <hamidne@mail.ru>
2019-01-08 16:53:02 +0330

    tokenize class from data

--- main.go
+++ main.go
@@ -2,15 +2,22 @@ package main
 
 import "fmt"
 
-func main()  {
-	var train [5]string
+func main() {
+	var train [4]string
 
-	train[1] = "a tehran iran tehran tehran iran"
-	train[2] = "a isfahan iran isfahan"
-	train[3] = "a iran shiraz fars"
-	train[4] = "b fars isfahan shiraz"
+	train[0] = "a tehran iran tehran tehran iran"
+	train[1] = "a isfahan iran isfahan"
+	train[2] = "a iran shiraz fars"
+	train[3] = "b fars isfahan shiraz"
 
 	var test = "iran isfahan shiraz tehran"
+
+	for _, train := range train {
+		fmt.Println("original : ", train)
+		fmt.Println("class : ", train[0:1])
+		fmt.Println("data : ", train[2:])
+		fmt.Println()
+	}
 
 	fmt.Println(test)
 }
