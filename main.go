176eb637611b242519843642727b196968708f8b
HamidNE <hamidne@mail.ru>
2019-01-20 19:02:56 +0330

    edit for naive base codes

--- main.go
+++ main.go
@@ -8,7 +8,45 @@ import (
 	"regexp"
 )
 
-func getTrainData() []string {
+func naiveBaise(data []string, test string) map[byte]float32 {
+
+	classCount := make(map[byte]byte)
+	itemCount := make(map[string]byte)
+	regex := regexp.MustCompile(`(?m) \w+`)
+	processedData := make(map[byte]map[string]byte)
+
+	for _, train := range data {
+
+		class := []byte(train[0:1])[0]
+		classCount[class]++
+
+		if len(processedData[class]) == 0 {
+			processedData[class] = map[string]byte{}
+		}
+
+		for _, match := range regex.FindAllString(train[1:], -1) {
+			itemCount[match]++
+			processedData[class][match]++
+		}
+	}
+
+	result := make(map[byte]float32)
+
+	for _, match := range regex.FindAllString(test, -1) {
+		for class := range processedData {
+			classCount[class] *= float32(processedData[class][match]+1) / float32(len(uniqueCity)+cityCount[class])
+		}
+	}
+
+	for class := range processedData {
+		fmt.Println(string(class), classCount[class]/float32(len(data)))
+	}
+
+	return result
+}
+
+func getData() []string {
+
 	var train []string
 	file, err := os.Open("./input.txt")
 
@@ -31,41 +69,11 @@ func getTrainData() []string {
 
 func main() {
 
-	var regex = regexp.MustCompile(`(?m) \w+`)
-
-	cityCount := make(map[byte]int)
 	uniqueCity := make(map[string]bool)
 	classCount := make(map[byte]float32)
-	processedData := make(map[byte]map[string]byte)
 
-	trainData := getTrainData()
-
-	for _, train := range trainData {
-
-		class := []byte(train[0:1])[0]
-		classCount[class]++
-
-		if len(processedData[class]) == 0 {
-			processedData[class] = map[string]byte{}
-		}
-
-		for _, match := range regex.FindAllString(train[1:], -1) {
-			cityCount[class]++
-			uniqueCity[match] = true
-			processedData[class][match]++
-		}
-	}
+	trainData := getData()
 
 	var test = " Chinese Chinese Chinese Tokyo Japan"
-
-	for _, match := range regex.FindAllString(test, -1) {
-		for class := range processedData {
-			classCount[class] *= float32(processedData[class][match]+1) / float32(len(uniqueCity)+cityCount[class])
-		}
-	}
-
-	for class := range processedData {
-		fmt.Println(class, classCount[class]/float32(len(trainData)))
-	}
 
 }
